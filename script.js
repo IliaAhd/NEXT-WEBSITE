ea78449d62dce29c3f8158fa86ba83af572db3d4
IliaNoah <vaguaman86@gmail.com>
2023-07-01 13:13:28 +0330

    added date

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Ham Menu
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar-menu");
 
@@ -5,3 +6,9 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// Date
+const dateEl = document.querySelector(".date");
+const date = new Date();
+
+dateEl.textContent = date.getFullYear();
